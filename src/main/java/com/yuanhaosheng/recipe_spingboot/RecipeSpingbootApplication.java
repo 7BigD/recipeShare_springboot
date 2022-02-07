c616519b26895f81a75fb495e6ac342ef1a85d3a
yuanhaosheng <yuanhaosheng@yuanhaosheng.lan>
2022-02-07 11:36:30 +0800

    init项目

---
+++ src/main/java/com/yuanhaosheng/recipe_spingboot/RecipeSpingbootApplication.java
@@ -0,0 +1,13 @@
+package com.yuanhaosheng.recipe_spingboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeSpingbootApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(RecipeSpingbootApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/yuanhaosheng/recipe_spingboot/RecipeSpingbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yuanhaosheng.recipe_spingboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeSpingbootApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
